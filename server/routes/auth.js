4f51812cccb66e82147c2c25924daff204c4d516
Sebastian Zapata Gutierrez <sebastian.zapata.g@gmail.com>
2024-09-03 18:20:53 +0100

    Add: auth routes

---
+++ server/routes/auth.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const router = express.Router();
+
+// Instantiate Services
+const AuthService = require("../services/AuthService");
+const AuthServiceInstance = new AuthService();
+
+module.exports = (app, passport) => {
+
+   app.use("/auth", router);
+
+   // Registration Endpoint
+   router.post("/register", async (req, res) => {
+
+      try {
+
+         const data = req.body;
+
+         const response = await AuthServiceInstance.register(data);
+
+         res.status(200).send(response);
+         
+      } catch (err) {
+         next(err);
+      }
+   });
+
+   // Login Endpoint
+   router.post("/login", async (req, res) => { // TODO: Add middleware to check if user is already logged in
+
+      try {
+
+         const { username, password } = req.body;
+
+         const response = await AuthServiceInstance.login({ username, password });
+
+         res.status(200).send(response);
+
+      } catch (err) {
+         next(err);
+      }
+   });
+
+
+}
