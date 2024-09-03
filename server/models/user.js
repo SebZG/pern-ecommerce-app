9744d623b5e5865112788d2ad59a3a6b2087f978
Sebastian Zapata Gutierrez <sebastian.zapata.g@gmail.com>
2024-09-03 17:35:10 +0100

    Add: user model

---
+++ server/models/user.js
@@ -0,0 +1,117 @@
+const db = require("../db");
+const pgp = require("pg-promise")(); //({ capSQL: true });
+
+module.export = class UserModel {
+
+   /**
+    * Creates a new user record
+    * @param {object}         data [User data]
+    * @return {Object|null}        [Created user record]
+    */
+
+   async create(data) {
+      try {
+         // Generate SQL statement - using helper for dynamic parameter injection
+         const statement = pgp.helpers.insert(data, null, "users") + "RETURNING *";
+
+         // Execute SQL statement
+         const result = await db.query(statement);
+
+         if (result.rows?.length) {
+            return result.rows[0];
+         }
+
+         return null;
+
+      } catch (err) {
+         throw new Error(err);
+      }
+   }
+
+   /**
+    * Updates a user record
+    * @param {Object}         data [User data]
+    * @return {Object|null}        [Updated user record]
+    */
+
+   async update(data) {
+      try {
+
+         const { id, ...params } = data;
+
+         // Generate SQL statement - using helper for dynamic parameter injection
+         const condition = pgp.as.format(`WHERE id = ${id} RETURNING *`);
+         const statement = pgp.helpers.update(params, null, "users") + condition;
+
+         // Execute SQL statement
+         const result = await db.query(statement);
+
+         if (result.rows?.length) {
+            return result.rows[0];
+         }
+
+         return null;
+
+      } catch (err) {
+         throw new Error(err);
+      }
+   }
+
+   /**
+    * Finds a user by email
+    * @param {string}         email [User email]
+    * @return {Object|null}         [User record]
+    */
+
+   async findByEmail(email) {
+      try {
+
+         // Generate SQL statement
+         const statement = `SELECT *
+                            FROM users
+                            WHERE email = $1`;
+         const values = [email];
+
+         // Execute SQL statement
+         const result = await db.query(statement, values);
+
+         if (result.rows?.length) {
+            return result.rows[0];
+         }
+
+         return null;
+
+      } catch (err) {
+         throw new Error(err);
+      }
+   }
+
+   /**
+    * Finds a user record by ID
+    * @param {string}         id [User ID]
+    * @return {Object|null}      [User record]
+    */
+
+   async findOneById(id) {
+      try {
+
+         // Generate SQL statement
+         const statement = `SELECT *
+                            FROM users
+                            WHERE id = $1`;
+         const values = [id];
+
+         // Execute SQL statement
+         const result = await db.query(statement, values);
+
+         if (result.rows?.length) {
+            return result.rows[0];
+         }
+
+         return null;
+
+      } catch (err) {
+         throw new Error(err);
+      }
+   }
+}
