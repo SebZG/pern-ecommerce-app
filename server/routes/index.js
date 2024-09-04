c8f152bebef62dec63a5891198d7e3ed8e687af0
Sebastian Zapata Gutierrez <sebastian.zapata.g@gmail.com>
2024-09-04 11:21:01 +0100

    Add: all routes @ routes/index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,13 @@
 const authRouter = require("./auth");
 const productRouter = require("./product");
+const userRouter = require("./user");
+const cartRouter = require("./router");
+const orderRouter = require("./order");
 
 module.exports = (app, passport) => {
    authRouter(app, passport);
    productRouter(app);
+   userRouter(app);
+   cartRouter(app);
+   orderRouter(app);
 }
